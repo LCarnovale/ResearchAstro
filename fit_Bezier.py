b0e3ac12ad3e52542db7e05197025b9aeb7e8537
Leo Carnovale <Leo.carnovale@gmail.com>
2019-12-18 11:32:32 +1100

    Clean up fit_bezier.py

--- fit_Bezier.py
+++ fit_Bezier.py
@@ -1,133 +1,14 @@
+# Author: Leo Carnovale (leo.carnovale@gmail.com)
+
+"""
+Functions to fit an arbitrary order Bezier curve to an arbitrary path.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 from scipy.special import comb
 
-import tools
-
-
-def get_track(mass, fe_h, a_fe=0, v_vcrit=0):
-    """ Fetch the evolutionary track of a star.
-    'Mass' is the initial mass, in solar masses.
-    'fe_h' is the metalicity of the star, 0 for a sun like star.
-    'a_fe' is the alpha to iron abundance, 0 for a sun like star.
-    'v_vcrit' is the rotation of the star as a ratio of the critical rotation.
-    MIST only includes rotation values of 0 and 0.4.
-
-    Returns a dictionary with all 77 fields from the table if it can be found.
-    """
-
-    m_str = f"{int(mass*1e2):05d}"
-    
-    feh = (fe_h<0 and 'm') or (fe_h>=0 and 'p')
-    feh = f'{feh}{abs(fe_h):.02f}'
-    afe = (a_fe<0 and 'm') or (a_fe>=0 and 'p')
-    afe = f'{afe}{abs(a_fe):.01f}'
-
-    fname = f"MistTracks/MIST_v1.2_feh_{feh}_afe_{afe}_vvcrit{v_vcrit:.01f}_EEPS/{m_str}M.track.eep"
-    print(f"Fetching: {fname}")
-    try:
-        data = np.genfromtxt(fname)    # The table data.
-        data = data.T
-        # Get headers etc.
-        with open(fname, 'r') as f:
-            lines = f.readlines()
-            headers = lines[11][1:-1]
-            remove_spaces = lambda x: [i for i in x.split(' ') if i]
-            headers = remove_spaces(headers)
-
-            EEPs = lines[8][2:-1]
-            EEPs = [int(x)-1 for x in EEPs[5:].split(' ') if (x and int(x) <= len(data[0]))]
-
-            inits = remove_spaces(lines[3][1:-1])
-            inits += remove_spaces(lines[6][1:-1])
-            init_vals = remove_spaces(lines[4][1:-1])
-            init_vals += remove_spaces(lines[7][1:-1])
-            init_vals = [float(x) for x in init_vals[:-2]] + init_vals[-2:]
-
-    except FileNotFoundError:
-        print("No track found for:")
-        print("mass:", mass)
-        print("fe_h:", fe_h)
-        print("a_fe:", a_fe)
-        print("v_vcrit:", v_vcrit)
-        return None
-    else:
-        pass
-        
-    out = dict()
-    
-    for init, val in zip(inits, init_vals):
-        out[init] = val
-    
-    out["EEPs"] = EEPs
-
-    for i, h in enumerate(headers):
-        out[h] = data[i]
-    
-    
-    return out
-    
-def get_phase_points(*args):
-    """ 
-    Usage:
-        get_phase_points(mass, fe_h)
-        get_phase_points(track)
-
-    Return an array of Temp-Lum pairs representing the
-    positions of evolutionary points along the requested track.
-    """
-    if len(args) >= 2:
-        track = get_track(*args[:2])
-    else:
-        track = args[0]
-
-    E_ax = track['EEPs'][:]
-    if len(E_ax) <= 7:
-        print("this one bad: M and Fe/H =", track['initial_mass'], track['[Fe/H]'])
-
-    t_ax = track['log_Teff']
-    L_ax = track['log_L']
-
-    # Try to find 'local inflexion' points
-    rise = L_ax[1:] - L_ax[:-1]
-    run  = t_ax[1:] - t_ax[:-1]
-    gradient = rise / run
-
-    # Select phase start points of a section (ie the EEP number)
-    # to analyse. 
-    slices_to_check = [slice(3,5), slice(6,4,-1), slice(9, 7, -1)]
-    for slic in slices_to_check:
-        rise = np.diff(L_ax[E_ax[slic]])[0]
-        run  = np.diff(t_ax[E_ax[slic]])[0]
-        phase_grad = rise / run
-
-        idx, idx_next = E_ax[slic]
-
-        # Cut out the section to be analysed
-        section = gradient[idx:idx_next:(-1 if idx>idx_next else 1)]
-        diff = section - phase_grad
-        # Negate the diff if the initial slope is positive
-        diff[2] < 0 or diff.__imul__(-1)
-        try:
-            min_idx = tools.getFromTrigger(diff, 0)[0]
-        except Exception as e:
-            plt.plot(diff)
-            plt.show()
-            raise e
-        if (idx > idx_next):
-            # Swap if we are looking at a reversed slice
-            idx, idx_next = idx_next, idx
-
-        # Add the new inflexion point
-        E_ax = np.append(E_ax, [min_idx+idx])
-        # E_ax = [*E_ax[:phase+1], min_idx + idx, *E_ax[phase+1:]]
-
-    
-    E_T = t_ax[E_ax]
-    E_L = L_ax[E_ax]
-
-    return np.array([E_T, E_L]).T
 
 def A(row, col, size):
     """Get the value of the element in a 
@@ -146,23 +27,20 @@ def A(row, col, size):
 
     return out
 
-generated = {}
+_generated = {}
 def generate_matrix(N):
     """ Generate a coeffecient matrix of size `N` for the calculation
     of Bezier curves. This returns `M` in `B(t) = T(t) * M * P`."""
-    global generated
-    if N in generated:
-        return generated[N]
+    global _generated
+    if N in _generated:
+        return _generated[N]
     else:
         rows = np.array([[row for col in range(N)] for row in range(N)])
         cols = np.array([[col for col in range(N)] for row in range(N)])
         M = A(rows, cols, N)
-        generated[N] = M
+        _generated[N] = M
         return M
     
-
-    
-
 def T(t, n):
     """ Return an array of `[1, t^1, t^2, ..., t^(n-1)]`
     for every value of `t`. `t` can be scalar or a 1-D array.
@@ -173,7 +51,6 @@ def T(t, n):
         return a.reshape(size, -1, 1)
     else:
         return a.reshape(-1, 1)
-
 
 def interp_path(path, distance, normalised=False):
     """ Interpolate along a given path to find the location of
@@ -295,26 +172,19 @@ def plot_curve(control_points, ax=None):
     points = curve_eval(t_ax, control_points)
     return ax.plot(*points.T)
 
-# Fit somehow???????????????
+if __name__ == "__main__":
+    path = np.array([np.linspace(1, 3, 100), np.linspace(1, 3, 100)**2]).T
+    conpoints = np.array([[1, 1], [3, 1], [3, 3], [1, 3]])
 
-track = get_track(1, 0, 0, 0.4)
+    fig = plt.figure()
+    ax1, ax2 = fig.subplots(ncols=2)
 
-EEPs = get_phase_points(track)
+    ax1.plot(*path.T)
+    ax1.plot(*conpoints.T)
+    plot_curve(conpoints, ax1)
 
-path = np.array([np.linspace(1, 3, 100), np.linspace(1, 3, 100)**2]).T
-conpoints = np.array([[1, 1], [3, 1], [3, 3], [1, 3]])
+    conpoints_opt = fit_curve(path, conpoints)[0]
 
-# get_dist_path_curve(np.array([0., 0.1]), path, conpoints)
-
-fig = plt.figure()
-ax1, ax2 = fig.subplots(ncols=2)
-
-ax1.plot(*path.T)
-ax1.plot(*conpoints.T)
-plot_curve(conpoints, ax1)
-
-conpoints_opt = fit_curve(path, conpoints)[0]
-
-ax2.plot(*path.T)
-ax2.plot(*conpoints_opt.T)
-plot_curve(conpoints_opt, ax2)
+    ax2.plot(*path.T)
+    ax2.plot(*conpoints_opt.T)
+    plot_curve(conpoints_opt, ax2)
